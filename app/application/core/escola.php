e3ad59f5173c7e301c471ed1140aea9680f9c506
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-03-15 13:42:30 -0300

    Atualização core escola (2ª versão)

--- app/application/core/escola.php
+++ app/application/core/escola.php
@@ -7,13 +7,16 @@ class Escola
 	
 	public function __construct($nome, $telefone)
 	{
-		$this->nome = $nome;
-		$this->telefone = $telefone;
+		$this->nome = trim(strip_tags($nome));
+		$this->telefone = trim(strip_tags($telefone));
 	}
 
 	public static function createObjectEscola ($resultRow) // Criando o Objeto <<Escola>>
 	{
-		return new Escola($resultRow['nome'], $resultRow['telefone']);
+		if(self::valida($resultRow)){
+			return new Escola($resultRow['nome'], $resultRow['telefone']);
+		}
+		return null;
 	}
 
 	public function getDataToSave()
@@ -24,6 +27,29 @@ class Escola
 	public function getSqlToInsert()
 	{
 		return 'INSERT INTO escola (nome, telefone) VALUES (?,?)';
+	}
+
+	public function getSqlToUpdate()
+	{
+		return 'UPDATE escola
+				SET nome=?, telefone=?
+				WHERE nome=?;';
+	}
+
+	public function getSqlToDelete()
+	{
+		return 'DELETE FROM escola
+				WHERE nome=?;';
+	}
+
+	public static function getSqlToSelect()
+	{
+		return 'SELECT * FROM escola WHERE nome=?;';
+	}
+
+	public static function getSqlToSelectAll()
+	{
+		return 'SELECT * FROM escola;';
 	}
 
 	public function getNome()
@@ -45,6 +71,46 @@ class Escola
 	{
 		$this->telefone = $newTelephone;
 	}
+}
+
+private static function valida ($data) 
+{
+        $errors = array();
+        $errors = self::validaNome($data['nome'], $errors);
+        $errors = self::validaTelefone($data['telefone'], $errors);
+
+        if($errors == null)
+        {
+            return true;
+        }
+
+        foreach ($errors as $error) 
+        {
+        	echo $error;
+        }
+        die();
+        return false;
+    }
+
+    private static function validaNome($data, $errors)
+    {
+        if (empty($data)) 
+        {
+            //tratar erro para campo nome vazio.
+            $errors['nome'] = "O campo Nome não pode estar vazio!";
+        } 
+        return $errors;
+    }
+
+	private static function validaTelefone($data, $errors)
+	{
+        if (empty($data)) 
+        {
+            //tratar erro para campo vinculo vazio.
+            $errors['telefone'] = "O campo Telefone não pode estar vazio!";
+        } 
+        return $errors;
+    }
 }
 
 ?>

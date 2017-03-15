1fb881aa066bc1b3bcbaa87e9786f45bb3bfc177
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-03-15 13:50:20 -0300

    Atualização do model Escola (versão 2)

--- app/application/models/Escola_model.php
+++ app/application/models/Escola_model.php
@@ -21,6 +21,48 @@ class Escola_model extends RN_Model
 
         return false;
     }
+
+    public function updateEscola($info)
+    {
+        $this->Logger->info("Running: " . __METHOD__);
+        $escola = Escola::createObjectEscola($info);
+        if(isset($escola))
+        {
+            $this->execute($this->db, $escola->getSqlToUpdate(), $escola->getDataToSave());
+        }
+        return false;
+    }
+    
+    public function deleteEscola($info)
+    {
+        $this->Logger->info("Running: " . __METHOD__);
+        print_r($info);
+        die();
+        $escola = Escola::createObjectEscola($info);
+        if(isset($escola)){
+            $this->execute($this->db, $escola->getSqlToDelete(), $escola->getName());
+        }
+        return false;
+    }
+
+    public function selectOneEscola($dataIn)
+    {
+        $this->Logger->info("Running: " . __METHOD__);
+        $result = $this->executeRow($this->db, Escola::getSqlToSelect(), $dataIn);
+        if ($result)
+            return Escola::createObjectEscola(json_decode(json_encode($result), true));
+        return null;  
+    }
+
+    public function selectAllEscola()
+    {
+        $this->Logger->info("Running: " . __METHOD__);
+        $result = $this->executeRows($this->db, Escola::getSqlToSelectAll());
+        $return_array = array();
+        foreach($result as $row)
+            $return_array[] = Escola::createObjectEscola(json_decode(json_encode($row), true));
+        return $return_array;
+    }    
 }
 
 ?>

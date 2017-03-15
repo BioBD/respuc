cffe01a7473cbc37812137216962134ab2b66d47
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-03-15 13:53:37 -0300

    Atualização da view insert da escola (2ª versão)

---
+++ app/application/views/escola/insert.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<form method="post" action="save" enctype="multipart/form-data">
+	<br>
+ 	Nome:
+  <input type="text" name="nome"><br><br>
+	Telefone:
+  <input type="text" name="telefone"><br><br>
+  <input type="submit" value="Submit">
+</form>

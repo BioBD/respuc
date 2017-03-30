be5de113fc186398ddb0c8bfa2b527dc791f404e
Daniel Cunha Rios <daniel@macbook-pro-de-daniel.local>
2017-03-29 23:07:44 -0300

    Criação da view aprendiz/show_aprendiz

---
+++ app/application/views/aprendiz/show_aprendiz.php
@@ -0,0 +1,26 @@
+<?php
+			echo "<br>";
+			echo "Mostrar Aprendiz: ";
+			echo $aprendiz->getNome()."<br>"; 
+			echo "CPF: ".$aprendiz->getCPF()."<br>";
+			echo "RG: ".$aprendiz->getRG()."<br>";
+			echo "Data de Nascimento: ".$aprendiz->getDataNascimento()."<br>";
+			echo "Naturalidade: ".$aprendiz->getNaturalidade()."<br>";
+			echo "E-mail: ".$aprendiz->getEmail()."<br>";
+			echo "Telefone: ".$aprendiz->getTelefone()."<br>";
+			echo "Celular: ".$aprendiz->getCelular()."<br>";
+			echo "Rua: ".$aprendiz->getRua()."<br>";
+			echo "Número: ".$aprendiz->getNumero()."<br>";
+			echo "Complemento: ".$aprendiz->getComplemento()."<br>";
+			echo "Bairro: ".$aprendiz->getBairro()."<br>";
+			echo "Cidade: ".$aprendiz->getCidade()."<br>";
+			echo "UF: ".$aprendiz->getUF()."<br>";
+			echo "Cidade: ".$aprendiz->getCidade()."<br>";
+			echo "CEP: ".$aprendiz->getCEP()."<br>";
+			echo "Cursos: ".$aprendiz->getCursos()."<br>";
+			echo "Informações do responsável"
+			echo "Nome: ".$aprendiz->getNomeResponsavel()."<br>";
+			echo "Telefone: ".$aprendiz->getTelefoneResponsavel()."<br>";
+			echo "Profissão: ".$aprendiz->getProfissaoResponsavel()."<br>";
+			echo "CPF: ".$aprendiz->getCPFResponsavel()."<br>";
+?>

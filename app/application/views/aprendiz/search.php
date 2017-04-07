08eccc5c81e9a748ae144ea323b78dfd25de39b8
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-04-07 19:19:26 -0300

    Primeira versão da view Search de Aprendiz

---
+++ app/application/views/aprendiz/search.php
@@ -0,0 +1,10 @@
+<?php
+?>
+
+<form method="post" action="find" enctype="multipart/form-data">
+	<br>
+ 	Nome:
+  <input type="search" name="nome"><br><br>
+  <input type="submit" value="Submit">
+</form>
+

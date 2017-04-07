1b98f08728dfe19afcd61244134dff729bf13eb2
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-04-07 19:19:54 -0300

    Primeira versão das views Show de Aprendiz

--- app/application/views/aprendiz/show_aprendiz.php
+++ app/application/views/aprendiz/show_aprendiz.php
@@ -1,4 +1,37 @@
 <?php
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+			echo "<br>";
+			echo "Mostrar Aprendiz: "; "<br>";
+
+			echo "Nome: ".$aprendiz->getNome()."<br>";
+			echo "CPF: ".$aprendiz->getCpf()."<br>";
+			echo "RG: ".$aprendiz->getRg()."<br>";
+			echo "Data de Nascimento: ".$aprendiz->getDataNascimento()."<br>";
+			echo "Naturalidade: ".$aprendiz->getNaturalidade()."<br>";
+			echo "Email: ".$aprendiz->getEmail()."<br>";
+			echo "Telefone: ".$aprendiz->getTelefone()."<br>";
+			echo "Celular: ".$aprendiz->getCelular()."<br>";
+			echo "Rua: ".$aprendiz->getRua()."<br>";
+			echo "Numero: ".$aprendiz->getNumero()."<br>";
+			echo "Complemento: ".$aprendiz->getComplemento()."<br>";
+			echo "Bairro: ".$aprendiz->getBairro()."<br>";
+			echo "Cidade: ".$aprendiz->getCidade()."<br>";
+			echo "UF: ".$aprendiz->getUf()."<br>";
+			echo "CEP: ".$aprendiz->getCep()."<br>";
+			echo "Trabalho: ".$aprendiz->getTrabalho()."<br>";
+
+			echo "<br>";
+			echo "Responsavel do Aprendiz: "; "<br>";
+
+			echo "Nome do Responsavel: ".$aprendiz->getNomeResponsavel()."<br>";
+			echo "Telefone do Responsavel: ".$aprendiz->getTelefoneResponsavel()."<br>";
+			echo "Profissao do Responsável: ".$aprendiz->getProfissaoResponsavel()."<br>";
+			echo "CPF do Responsavel: ".$aprendiz->getCpfResponsavel()."<br>";
+=======
+>>>>>>> Stashed changes
 			echo "<br>";
 			echo "Mostrar Aprendiz: ";
 			echo $aprendiz->getNome()."<br>"; 
@@ -23,4 +56,8 @@
 			echo "Telefone: ".$aprendiz->getTelefoneResponsavel()."<br>";
 			echo "Profissão: ".$aprendiz->getProfissaoResponsavel()."<br>";
 			echo "CPF: ".$aprendiz->getCPFResponsavel()."<br>";
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/development
+>>>>>>> Stashed changes
 ?>
--- app/application/views/aprendiz/show_aprendizes.php
+++ app/application/views/aprendiz/show_aprendizes.php
@@ -1,3 +1,51 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+<?php
+	foreach ($aprendizes as $aprendiz) 
+	{
+			echo "<br>";
+			echo "Mostrar Aprendiz: "; "<br>";
+
+			echo "Nome: ".$aprendiz->getNome()."<br>";
+			echo "CPF: ".$aprendiz->getCpf()."<br>";
+			echo "RG: ".$aprendiz->getRg()."<br>";
+			echo "Data de Nascimento: ".$aprendiz->getDataNascimento()."<br>";
+			echo "Naturalidade: ".$aprendiz->getNaturalidade()."<br>";
+			echo "Email: ".$aprendiz->getEmail()."<br>";
+			echo "Telefone: ".$aprendiz->getTelefone()."<br>";
+			echo "Celular: ".$aprendiz->getCelular()."<br>";
+			echo "Rua: ".$aprendiz->getRua()."<br>";
+			echo "Numero: ".$aprendiz->getNumero()."<br>";
+			echo "Complemento: ".$aprendiz->getComplemento()."<br>";
+			echo "Bairro: ".$aprendiz->getBairro()."<br>";
+			echo "Cidade: ".$aprendiz->getCidade()."<br>";
+			echo "UF: ".$aprendiz->getUf()."<br>";
+			echo "CEP: ".$aprendiz->getCep()."<br>";
+			echo "Trabalho: ".$aprendiz->getTrabalho()."<br>";
+
+			echo "<br>";
+			echo "Responsavel do Aprendiz: "; "<br>";
+
+			echo "Nome do Responsavel: ".$aprendiz->getNomeResponsavel()."<br>";
+			echo "Telefone do Responsavel: ".$aprendiz->getTelefoneResponsavel()."<br>";
+			echo "Profissao do Responsável: ".$aprendiz->getProfissaoResponsavel()."<br>";
+			echo "CPF do Responsavel: ".$aprendiz->getCpfResponsavel()."<br>";
+
+			echo '<form method="post" action="update" enctype="multipart/form-data">';
+			echo '<input type="submit" name="update-btn" value="Edit">'; echo "</form>";
+			echo '<a href=<?$config["base_url"]?>index.php/AprendizController/delete?nome='.aprendiz->getNome();
+			echo "DELETE";
+			echo "</a>";
+
+			/* 	Não está funcionando.
+			echo '<form method="post" action="delete" enctype="multipart/form-data">';
+			echo '<input type="submit" name="delete-btn" value="Delete">';
+			echo "</form>";
+			*/
+=======
+>>>>>>> Stashed changes
 <?php
 	foreach ($aprendizes as $aprendiz) {
 			echo "<br>";
@@ -30,5 +78,9 @@
 			echo '<a href=<?$config["base_url"]?>index.php/instituicaocontroller/delete?nome='.$instituicao->getNome();
 			echo "DELETE";
 			echo "</a>";
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/development
+>>>>>>> Stashed changes
 		}
 ?>

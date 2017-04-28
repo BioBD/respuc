e3475fa79dc50d9566cc4ae54c3944b45bd5186c
Pedro Escalfoni <pedro_escalfoni@hotmail.com>
2017-04-28 02:48:01 -0300

    Update show_funcionarios.php

--- app/application/views/funcionario/show_funcionarios.php
+++ app/application/views/funcionario/show_funcionarios.php
@@ -2,7 +2,7 @@
 	foreach ($funcionarios as $funcionario) 
 	{
 			echo "<br>";
-			echo "Mostrar Funcionario: "; "<br>";
+			echo "Mostrar Funcionairo: "; "<br>";
 			echo "Nome: ".$funcionario->getNome()."<br>";
 			echo "CPF: ".$funcionario->getCpf()."<br>";
 			echo "RG: ".$funcionario->getRg()."<br>";
@@ -12,6 +12,8 @@
 			echo "Telefone: ".$funcionario->getTelefone()."<br>";
 			echo "Celular: ".$funcionario->getCelular()."<br>";
 			echo "Funcao: ".$funcionario->getFuncao()."<br>";
+			$String = "{$this->config->item('base_link')}FuncionarioController/edit?nome=".$funcionario->getNome();
+			$String2 = "{$this->config->item('base_link')}FuncionarioController/delete?nome=".$funcionario->getNome();
 			echo '<a href="'.$String.'">EDIT</a>';
 			echo "<br>";
 			echo '<a href="'.$String2.'">DELETE</a>';

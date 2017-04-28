c085903d927be6f242d21f40778ce8756e048bfb
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-04-28 17:56:10 -0300

    Fix CRUD de Curso

--- app/application/views/curso/search.php
+++ app/application/views/curso/search.php
@@ -4,12 +4,6 @@
 	<br>
  	Nome:
   <input type="search" name="nome"><br><br>
-  	Coordenador:
-  <input type="search" name="coord"><br><br>
-  	Departamento:
-  <input type="search" name="depto"><br><br>
-  	Quantidade de Alunos:
-  <input type="search" name="qtd_alunos"><br><br>
   <input type="submit" value="Submit">
 </form>
 

fc4748ea9ab2f90b3c41fd7c51eff7491b1ea1ed
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-05-21 12:46:05 -0300

    Fix and test index website

--- app/application/views/include/left_menu.php
+++
@@ -1,9 +0,0 @@
-<div class="col-lg-2  left-action-bar">
-    <ul class="nav nav-pills nav-stacked">
-		<li><a href="<?= $this->config->item('base_link'); ?>InstituicaoController/home">Instituiçao</a></li>
-		<li><a href="<?= $this->config->item('base_link'); ?>EscolaController/home">Escola</a></li>
-		<li><a href="<?= $this->config->item('base_link'); ?>AprendizController/home">Aprendiz</a></li>
-    </ul>
-</div>
-
-

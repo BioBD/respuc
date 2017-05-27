cdde192550fa26f4bc8d1eb932f01b50c2d83ecb
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-05-26 22:56:31 -0300

    Implementação do CRUD Instituição para o site

--- app/application/views/instituicao/excluidosucesso.php
+++ app/application/views/instituicao/excluidosucesso.php
@@ -1,15 +1,6 @@
-<div class = "row">
-    <?php require_once APPPATH . 'views/include/left_menu.php' ?>
-    <div class="col-lg-10">
-    	<?php
-			echo "Instituição excluída com sucesso!";
-			echo "<br>";
-			echo "<a href='{$this->config->item('base_link')}InstituicaoController/show'>Voltar</a>";
-		?>
-
-    </div>
-</div>
-
-
-
-
+<script>function goBack() { window.history.back() } </script>
+<?php
+	echo "Aluno excluído com sucesso!";
+	echo "<br>";
+?>
+<input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>

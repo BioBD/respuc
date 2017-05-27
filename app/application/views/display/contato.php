c5e5c0d62d87ff1f3ba24f752c7371e336aa2a98
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-05-26 22:53:00 -0300

    Criação de controller e view de display do site

---
+++ app/application/views/display/contato.php
@@ -0,0 +1,96 @@
+<link rel="stylesheet" href="<?= $this->config->item('assets'); ?>/css/style.css">
+<link rel="stylesheet" href="<?= $this->config->item('assets'); ?>/css/style_alt.css" type="text/css">
+<div align="center" id="wrap" style="background-repeat: repeat-y; background-position: center center;">
+<table cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td valign="top">
+<div style="position: absolute; top: 2px; right: 13px; text-align:right">
+<table><tbody><tr></tr></tbody></table></div><br>
+<meta charset="UTF-8">
+<title></title>
+
+
+<div class="col-md-2"> </div>
+<div class="col-md-8">
+
+  <section id="cd-timeline" class="cd-container">
+    <div class="cd-timeline-block">
+      <div class="cd-timeline-img cd-picture bounce-in">
+            <img src="<?= $this->config->item('assets'); ?>images/favicon.png">
+      </div>
+
+      <div class="cd-timeline-content bounce-in">
+        <h5><strong>NEAM - Núcleo de Estudo e Ação sobre o Menor</strong></h5><br><br><br>
+
+                <span><i class="fa fa-user-circle" aria-hidden="true"> Endereço:</i></span><br><br>
+                <span>Rua Marquês de São Vicente, 225 - Gávea</span><br>
+                <span>Rio de Janeiro, RJ - Edifício Cardeal Leme</span><br><br>
+
+                <span><i class="fa fa-phone" aria-hidden="true"></i> Telefones:</span><br><br>
+                <span> Ana Cristina - Secretaria:</span><br>
+                <span> +55 (21) 3527-1368</span><br><br>
+                <span> Davison Coutinho - Assessoria:</span><br>
+                <span> +55 (21) 3527-1053</span> <br><br>
+          </div> 
+      </div>
+        
+      <div class="cd-timeline-block">
+        <div class="cd-timeline-img cd-picture bounce-in">
+          <img src="<?= $this->config->item('assets'); ?>images/favicon.png">
+        </div>
+            <div class="cd-timeline-content bounce-in">
+        <h5><strong>RESPUC - Rede de Empreendimentos Sociais</strong></h5><br><br><br>
+
+                <span><i class="fa fa-user-circle"> Endereço:</i></span><br><br>
+                <span>Rua Marquês de São Vicente, 225 - Gávea</span><br>
+                <span>Rio de Janeiro, RJ - Edifício Cardeal Leme</span><br><br>
+
+                <span><i class="fa fa-phone" aria-hidden="true"></i> Telefone:</span><br><br>
+                <span> Andrea Oliveira:</span><br>
+                <span> +55 (21) 3527-1056</span><br><br>
+
+                <span><i class="fa fa-envelope" aria-hidden="true"></i> Email: respucrio@puc-rio.br</span><br><br> 
+          </div> 
+      </div>
+        
+      <div class="cd-timeline-block">
+        <div class="cd-timeline-img cd-picture is-hidden"> 
+        <img src="<?= $this->config->item('assets'); ?>images/favicon.png">
+        </div>
+            <div class="cd-timeline-content is-hidden">
+        <h5><strong>Pontifícia Universidade Católica do Rio de Janeiro</strong></h5><br><br><br>
+
+                <span><i class="fa fa-user-circle" aria-hidden="true"> Endereço:</i></span><br><br>
+                <span>Rua Marquês de São Vicente, 225, Gávea</span><br> 
+                <span>Rio de Janeiro, RJ - Brasil - 22451-900</span><br> 
+                <span>Cx. Postal: 38097</span><br><br> 
+
+                <span><i class="fa fa-phone" aria-hidden="true"></i> Telefone: +55 (21) 3527-1001</span><br><br>
+          </div> 
+      </div>        
+
+      <div class="cd-timeline-block">
+        <div class="cd-timeline-img cd-picture is-hidden"> 
+        <img src="<?= $this->config->item('assets'); ?>images/favicon.png">
+        </div>
+            <div class="cd-timeline-content is-hidden">
+        <h5><strong>Representante da Equipe de Desenvolvimento do Sistema</strong></h5><br><br>
+
+                <span>Professor: Sérgio Lifschitz</span><br><br>
+                <span><i class="fa fa-user-circle" aria-hidden="true"> Endereço:</i></span><br><br>
+                <span>PUC-Rio Informática</span><br> 
+                <span>Rua Marquês de São Vicente 225</span><br> 
+                <span>Rio de Janeiro, RJ 22451-900, Brasil</span><br><br> 
+
+                <span><i class="fa fa-phone" aria-hidden="true"></i> Telefones:</span><br><br>
+                <span> +55 (21) 3527-1500 (ramal/extension 4346) </span><br><br>
+                <span> FAX: +55 (21) 3527-1530</span> <br><br>
+
+                <span><i class="fa fa-envelope" aria-hidden="true"></i> Email: sergio@inf.puc-rio.br</span><br><br>
+
+                <span><i class="fa fa-globe" aria-hidden="true"></i> Site: http://www-di.inf.puc-rio.br/~sergio/ </span><br><br>
+          </div> 
+      </div>        
+</section>
+
+<script src="<?= $this->config->item('assets'); ?>js/resourse.js"></script>
+
+</div></td></tr></tbody></table></div>

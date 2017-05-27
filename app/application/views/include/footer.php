ab95b5a9a62ecaa69e277bcd1b76bbd60887f853
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-05-26 22:53:48 -0300

    Implementação inicial do site

--- app/application/views/include/footer.php
+++ app/application/views/include/footer.php
@@ -1,17 +1,22 @@
- 		</div>
- 		<hr class="footer-hr" />
+<footer>
+<div class="container"> <hr class=""> <div class="row">
+<div class="col-md-12 text-center"> <ul class="list-inline">
+        
+<li><a href="https://www.facebook.com/BRPUCRio/" title="PUC-RIO Facebook" class="" target="_blank"> 
+<span class="fa fa-facebook-square fa-2x"> &nbsp; </span> </a> </li>
 
-		<div class="container footer-section">
-			<div class="row">
-				<div class="col-lg-6">
-					<p>SISTEMA RESPUC NEAM</p>
-				</div>
-				<div class="col-lg-6">
-					<p align="right">
-					EM DESENVOLVIMENTO
-					</p>
-				</div>
-			</div>
-		</div>
-	</body>
-</html>
+<li><a href="https://twitter.com/pucriodigital" title="PUC-RIO Twitter" class="" target="_blank"> 
+<span class="fa fa-twitter-square fa-2x"> &nbsp; </span> </a> </li> 
+
+<li><a href="https://br.linkedin.com/edu/pontifícia-universidade-católica-do-rio-de-janeiro-10582" title="PUC-RIO Twitter" class="" target="_blank"> 
+<span class="fa fa-linkedin-square fa-2x"> &nbsp; </span> </a> </li> 
+
+<li><a href="https://www.youtube.com/user/pucriooficial" title="PUC-RIO YouTube" class="" target="_blank"> 
+<span class="fa fa-youtube-square fa-2x"> &nbsp; </span> </a> </li> <br>
+        
+<p class=""> <a href="#" class="">© SISTEMA NEAM/RESPUC 2017 - Todos os Direitos Reservados. </a> </p> </ul> </div> </div> </div>
+    
+<iframe src="http://www.puc-rio.br/includes/rodape.html?fundo=claro" name="iRodape" width="100%" height="160" scrolling="no" frameborder="0" id="iRodape" allowtransparency="true"></iframe> </footer>
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="alert alert-info" role="alert"> <strong> <marquee> <i class="fa fa-warning"></i>SISTEMA EM DESENVOLVIMENTO!</strong> </marquee> </div> </body></html>

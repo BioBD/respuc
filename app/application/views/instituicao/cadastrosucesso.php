c60a7dfa39516219c4b48955ee9a3a25c0fbaf85
Carlos Eduardo Rosar Kós Lassance <cadurosar@gmail.com>
2017-07-21 10:55:11 -0300

    Revert "Implementação do CRUD Instituição para o site"
    This reverts commit c4885238bcd4d31c7a9e00f7d8521aa7fdb4a765.

--- app/application/views/instituicao/cadastrosucesso.php
+++ app/application/views/instituicao/cadastrosucesso.php
@@ -1,6 +1,16 @@
-<script>function goBack() { window.history.back() } </script>
-<?php
-	echo "Aluno cadastrado com sucesso!";
-	echo "<br>";
-?>
-<input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>
+<div class = "row">
+    <?php require_once APPPATH . 'views/include/left_menu.php' ?>
+    <div class="col-lg-10">
+		<?php
+			echo "Instituição cadastrada com sucesso!";
+			echo "<br>";
+			echo "<a href='{$this->config->item('base_link')}InstituicaoController/show'>Voltar</a>";
+		?>
+    </div>
+</div>
+
+
+
+
+
+
--- app/application/views/instituicao/insert.php
+++ app/application/views/instituicao/insert.php
@@ -1,8 +1,4 @@
 <script>
-    function goBack() {
-      window.history.back()
-    }
-
     /* permite apenas numeros, tab e backspace*/
     function validateNumberInput(evt) {
 
@@ -92,113 +88,144 @@
 
 </script>
 
-<style type="text/css">
-  h4 {color: black; margin-left: all}
-  label {color: black;}
-  .container{margin: auto;width: 45%;}
-  input[type=text]:focus { border: 2px solid #555;}
-  input[type=email]:focus { border: 2px solid #555;}
-</style>
+<div class = "row">
+    <?php // require_once APPPATH . 'views/include/left_menu.php' ?>
+    <div class="col-lg-12">
+      <form method="post" name="theForm" id="theForm" action="save" enctype="multipart/form-data">
 
-<div class="container"><br>
-  <h4><i class="fa fa-cog" aria-hidden="true"></i>  Inserir Nova Instituição <i class="fa fa-cog" aria-hidden="true"></i></h4><br>
-    <form method="post" id="theForm" action="save" enctype="multipart/form-data">
-      <div class="form-group">
-        <label for="nome" class="col-lg-1">Nome*: </label><br><br>
-        <input type="text" size="10" class="form-control" placeholder="Digite o nome da instituição"
-                  name="nome" onkeypress="return validateLetterInput(event);" required
-                  oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                          if (!empty($_POST['nome']))
-                                echo $_POST['nome'];
-        ?>"/> </div>
-
-    <div class="form-group">               
-        <label for="telefone" class="col-lg-1"> Telefone*: </label> <br><br>
-        <input type="text" class="form-control phone phone1" placeholder="(XX) XXXX-XXXX"
-                  name="telefone" id="telefone" maxlength="25" required onkeypress="return validateNumberInput(event);"
-                  oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                              if (!empty($_POST['telefone'])) 
-                                  echo $_POST['telefone'];
-              ?>"/></div>
-
-
-    <div class="form-group">
-        <label for="celular" class="col-lg-1 control-label"> Celular*: </label> <br><br>
-        <input type="text" class="form-control phone phone1" placeholder="(XX) XXXXX-XXXX"
-                  name="celular" id="celular" maxlength="25" required onkeypress="return validateNumberInput(event);"
-                  oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                              if (!empty($_POST['celular'])) 
-                                  echo $_POST['celular'];
-              ?>"/></div>
+        <div class="row">
+                <div class="form-group">
+                    <label for="nome" class="col-lg-1 control-label"> Nome*: </label>
+                    <div class="col-lg-4">
+                        <input type="text" class="form-control" placeholder="Digite o nome"
+                               name="nome" onkeypress="return validateLetterInput(event);" required
+                               oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
+                               oninput="setCustomValidity('')"
+                               value="<?php
+                               if (!empty($_POST['nome'])) {
+                                   echo $_POST['nome'];
+                               }
+                               ?>"/>
+                    </div>
                     
-
-    <div class="form-group">
-        <label for="email" class="col-lg-1 control-label"> Email*: </label> <br><br>
-        <input type="email" id="email" class="form-control" placeholder="XXXXXX@gmail.com"
-                  name="email" required title ="Favor incluir '@' e '.' ."
-                  oninvalid="this.setCustomValidity('Este campo requer um endereço de email.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                              if (!empty($_POST['email'])) {
-                                  echo $_POST['email'];
+                    <label for="telefone" class="col-lg-1 control-label"> Telefone*: </label>
+                    <div class="col-lg-3">
+                          <input type="text" class="form-control phone phone1" placeholder="(ddd) Telefone de contato"
+                                name="telefone" id="telefone" maxlength="25" required onkeypress="return validateNumberInput(event);"
+                                oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
+                                oninput="setCustomValidity('')"
+                                value="<?php
+                                if (!empty($_POST['telefone'])) {
+                                    echo $_POST['telefone'];
+                                }
+                                ?>"/>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <div class="row">
+                <div class="form-group">
+                    <label for="celular" class="col-lg-1 control-label"> Celular*: </label>
+                    <div class="col-lg-3">
+                          <input type="text" class="form-control phone phone1" placeholder="(ddd) Celular de contato"
+                                name="celular" id="celular" maxlength="25" required onkeypress="return validateNumberInput(event);"
+                                oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
+                                oninput="setCustomValidity('')"
+                                value="<?php
+                                if (!empty($_POST['celular'])) {
+                                    echo $_POST['celular'];
+                                }
+                                ?>"/>
+                    </div>
+                    
+                    <label for="email" class="col-lg-1 control-label"> E-mail*: </label>
+                    <div class="col-lg-3">
+                        <input type="email" id="email" class="form-control" placeholder="Email"
+                               name="email" required title ="Favor incluir '@' e '.' ."
+                               oninvalid="this.setCustomValidity('Este campo requer um endereço de email.')"
+                               oninput="setCustomValidity('')"
+                               value="<?php
+                               if (!empty($_POST['email'])) {
+                                   echo $_POST['email'];
                                }
-              ?>"/><script type="text/javascript"> window.onload = funcExistingEmail();</script> </div>                  
+                               ?>"/>
+                        <script type="text/javascript">
+                            window.onload = funcExistingEmail();
+                        </script>
+                    </div>
 
-    <div class="form-group">
-        <label for="vinculo" class="col-lg-1 control-label"> Vínculo: </label> <br><br>
-        <input type="text" class="form-control phone phone1" placeholder="Digite o vínculo"
-                  name="vinculo" id="vinculo" maxlength="25" required onkeypress="return validateNumberInput(event);"
-                  oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                              if (!empty($_POST['vinculo'])) 
-                                  echo $_POST['vinculo'];
-              ?>"/></div>
-
-  <h4><i class="fa fa-cog" aria-hidden="true"></i>  Dados do Responável <i class="fa fa-cog" aria-hidden="true"></i></h4><br>
-      <div class="form-group">
-          <label for="nome_responsavel" class="col-lg-1 control-label"> Nome: </label><br><br>
-          <input type="text" class="form-control" placeholder="Digite o nome do responsável"
-                  name="nome_responsavel" onkeypress="return validateLetterInput(event);" required
-                  oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                              if (!empty($_POST['nome_responsavel'])) 
-                                  echo $_POST['nome_responsavel'];
-          ?>"/> </div>
-
-
-      <div class="form-group">    
-          <label for="telefone_responsavel" class="col-lg-1 control-label"> Telefone*: </label><br><br>
-          <input type="text" class="form-control phone phone1" placeholder="(XX) XXXX-XXXX"
-                  name="telefone_responsavel" id="telefone_responsavel" maxlength="25" required onkeypress="return validateNumberInput(event);"
-                  oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                                if (!empty($_POST['telefone_responsavel'])) 
-                                    echo $_POST['telefone_responsavel'];
-          ?>"/></div>
-
-    <div class="form-group">
-        <label for="email_responsavel" class="col-lg-1 control-label"> Email*: </label> <br><br>
-        <input type="email_responsavel" id="email_responsavel" class="form-control" placeholder="XXXXXX@gmail.com"
-                  name="email_responsavel" required title ="Favor incluir '@' e '.' ."
-                  oninvalid="this.setCustomValidity('Este campo requer um endereço de email.')"
-                  oninput="setCustomValidity('')"
-                  value="<?php
-                              if (!empty($_POST['email_responsavel'])) {
-                                  echo $_POST['email_responsavel'];
+                    <label for="vinculo" class="col-lg-1 control-label"> Vínculo: </label>
+                    <div class="col-lg-3">
+                        <input type="text" class="form-control" placeholder="Digite o vínculo"
+                               name="vinculo" onkeypress="return validateLetterInput(event);" required
+                               oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
+                               oninput="setCustomValidity('')"
+                               value="<?php
+                               if (!empty($_POST['vinculo'])) {
+                                   echo $_POST['vinculo'];
                                }
-              ?>"/><script type="text/javascript"> window.onload = funcExistingEmail();</script> </div>         
+                               ?>"/>
+                    </div>                    
+                  </div>
+                </div>
+                <br/>
+                <b>Responsável:</b>
+                <div class="row"><br/></div>
+                <div class="row">
+                    <div class="form-group">
+                        <label for="nome_responsavel" class="col-lg-1 control-label"> Nome: </label>
+                        <div class="col-lg-3">
+                            <input type="text" class="form-control" placeholder="Digite o nome do responsável"
+                                name="nome_responsavel" onkeypress="return validateLetterInput(event);" required
+                                oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
+                                oninput="setCustomValidity('')"
+                                value="<?php
+                                if (!empty($_POST['nome_responsavel'])) {
+                                    echo $_POST['nome_responsavel'];
+                                }
+                                ?>"/>
+                        </div>
+                        
+                        <label for="telefone_responsavel" class="col-lg-1 control-label"> Telefone*: </label>
+                        <div class="col-lg-3">
+                            <input type="text" class="form-control phone phone1" placeholder="(ddd) Telefone do responsável"
+                                    name="telefone_responsavel" id="telefone_responsavel" maxlength="25" required onkeypress="return validateNumberInput(event);"
+                                    oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
+                                    oninput="setCustomValidity('')"
+                                    value="<?php
+                                    if (!empty($_POST['telefone_responsavel'])) {
+                                        echo $_POST['telefone_responsavel'];
+                                    }
+                                    ?>"/>
+                        </div>
 
-    <div class="row">
-      <div class="form-group">
-        <div class="col-lg-1"></div>
-        <input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>
-        <input class="btn btn-primary" type="submit" value="Salvar"> </div></div></form></div></div>
+                        <label for="email_responsavel" class="col-lg-1 control-label"> E-mail*: </label>
+                        <div class="col-lg-3">
+                            <input type="email_responsavel" id="email_responsavel" class="form-control" placeholder="Email do responsável"
+                                name="email_responsavel" required title ="Favor incluir '@' e '.' ."
+                                oninvalid="this.setCustomValidity('Este campo requer um endereço de email.')"
+                                oninput="setCustomValidity('')"
+                                value="<?php
+                                if (!empty($_POST['email_responsavel'])) {
+                                    echo $_POST['email_responsavel'];
+                                }
+                                ?>"/>
+                            <script type="text/javascript">
+                                window.onload = funcExistingEmail();
+                            </script>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="form-group">
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-4" style="padding-right:95px">
+                            <input class="btn btn-primary" style="float:right" type="submit" value="Salvar">
+                        </div>
+                    </div>
+                </div>
+      </form>
+    </div>
+</div>

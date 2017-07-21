db5eb2f0f9bd919dde1bbfe0158edb3cf7796588
Carlos Eduardo Rosar Kós Lassance <cadurosar@gmail.com>
2017-07-21 10:55:11 -0300

    Revert "Implementação do CRUD Aluno para o site"
    This reverts commit 8ca7da5d05397f1a38b3bfc7684765f64018b885.

--- app/application/views/aluno/admin.php
+++ app/application/views/aluno/admin.php
@@ -1,61 +1,57 @@
-<script></script> <br> <br>
-<div>
-	<?php 
-		if(isset($message) && $message !== null)
-			echo "<script>alert(\"{$message}\")</script>";			
-	?>
-
 <script>
-function goBack() {
-    window.history.back()
-}
+
 </script>
 
 <div>
-    <a href="<?php echo $this->config->item('base_link').'AlunoController/insert'?>">
-    <input type="button" class='btn btn-info' value="Cadastrar Aluno"></a>
+    <?php 
+		if(isset($message) && $message !== null){
+			echo "<script>alert(\"{$message}\")</script>";			
+		} 
+	?>
+    <div>
+            <a href="<?php echo $this->config->item('base_link').'AlunoController/insert' ?>">
+                <input type="button" class='btn btn-primary' value="Inserir Aluno">
+            </a>
+            <a href="<?php echo $this->config->item('base_link').'AlunoController/form_csv' ?>">
+                <input type="button" class='btn btn-success' value="Importar CSV">
+            </a>
 
-    <a href="<?php echo $this->config->item('base_link').'AlunoController/form_csv'?>">
-    <input type="button" class='btn btn-success' value="Importar CSV"></a>
+        <div class="row">
+        </div>
+        <br/>
 
-    <input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>
-
-    <div class="row"></div> <br>
-
-	<table class="table table-bordered table-striped table-min-td-size" style="max-width: 100%;" id="sortable-table">
-	<tr>
-		<th style="width: 100px;">Nome</th>
-		<th style="width: 100px;">Email</th>
-		<th colspan="2" style="width: 600px;">Responsável</th>
-        <th colspan="2" style="width: 500px;">Ações</th>
-	</tr>
-
-	<tr>
-		<th></th>
-		<th></th>
-		<th style="width: 100px;">Nome</th>
-		<th style="width: 250px;">Telefone</th>
-        <th style="width: 250px;"></th>
-        <th style="width: 250px;"></th> 
-	</tr>
-
-	<tbody id="tablebody">
-	<?php foreach ($alunos as $aluno) { ?> 
-	<tr>
-		<td>
-			<a href='<?=$this->config->item('base_link')?>AlunoController/get?cpf=<?=urlencode($aluno->getCPF())?>'>
-				<?=$aluno->getNome()?> </a> 
-				</td>
-					<td><?php echo $aluno->getEmail();?></td>
-						<td><?php echo $aluno->getNomeResponsavel();?></td>
-							<td><?php echo $aluno->getTelefoneResponsavel();?></td>
-                                <td><a href="<?php echo $this->config->item('base_link').'AlunoController/edit?cpf='.$aluno->getCPF(); ?>">
-                                <input class="btn btn-primary" type="button" value="Editar"></a></td>
-                                <td><a href="<?php echo $this->config->item('base_link').'AlunoController/delete?cpf='.$aluno->getCPF(); ?>">
-                                <input class="btn btn-danger" type="button" value="Excluir"></a></td>								
-								</tr>
-						<?php } ?>
-				</tbody>
-		</table>
+		<table class="table table-bordered table-striped table-min-td-size" style="max-width: 100%;" id="sortable-table">
+							<tr>
+								<th style="width: 100px;">Nome</th>
+								<th style="width: 100px;">Email</th>
+								<th colspan="2" style="width: 600px;">Responsável</th>
+                                <th colspan="2" style="width: 500px;">Ações</th>
+							</tr>
+							<tr>
+								<th></th>
+								<th></th>
+								<th style="width: 100px;">Nome</th>
+								<th style="width: 250px;">Telefone</th>
+                                <th style="width: 250px;"></th>
+                                <th style="width: 250px;"></th> 
+							</tr>
+							<tbody id="tablebody">
+								<?php foreach ($alunos as $aluno) { ?> 
+									<tr>
+										<td>
+											<a href='<?=$this->config->item('base_link')?>AlunoController/get?cpf=<?=urlencode($aluno->getCPF())?>'>
+												<?=$aluno->getNome()?> </a> 
+										</td>
+										<td><?php echo $aluno->getEmail();?></td>
+										<td><?php echo $aluno->getNomeResponsavel();?></td>
+										<td><?php echo $aluno->getTelefoneResponsavel();?></td>
+                                        <td><a href="<?php echo $this->config->item('base_link').'AlunoController/edit?cpf='.$aluno->getCPF(); ?>">
+                                            <input class="btn btn-success" type="button" value="Editar"></a></td>
+                                        <td><a href="<?php echo $this->config->item('base_link').'AlunoController/delete?cpf='.$aluno->getCPF(); ?>">
+                                            <input class="btn btn-danger" type="button" value="Deletar"></a></td>								
+									</tr>
+								<?php } ?>
+							</tbody>
+			</table>
     </div>
 </div>

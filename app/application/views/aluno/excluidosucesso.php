1f1a35d5fc93fbe5121f49129ce642deb65d0b03
Carlos Eduardo Rosar Kós Lassance <cadurosar@gmail.com>
2017-07-21 10:58:52 -0300

    Revert "Revert "Implementação do CRUD Aluno para o site""
    This reverts commit db5eb2f0f9bd919dde1bbfe0158edb3cf7796588.

--- app/application/views/aluno/excluidosucesso.php
+++ app/application/views/aluno/excluidosucesso.php
@@ -1,5 +1,6 @@
+<script>function goBack() { window.history.back() } </script>
 <?php
-	echo "Aluno excluído com sucesso !";
+	echo "Aluno excluído com sucesso!";
 	echo "<br>";
-	echo "<a href='{$this->config->item('base_link')}AlunoController/show'>Voltar</a>";
 ?>
+<input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>

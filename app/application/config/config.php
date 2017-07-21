9e47ff71f915e22a472d121f1902dc3c7516a193
Carlos Eduardo Rosar Kós Lassance <cadurosar@gmail.com>
2017-07-21 10:58:52 -0300

    Revert "Revert "Fix and test index website""
    This reverts commit d64dc161f2111b13d08ce9ab3f388427ec88cc77.

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/respuc/';
+$config['base_url'] = 'http://localhost/respuc/app/';
 $config['assets'] = $config['base_url'] . "assets/";
 $config['base_link'] = $config['base_url'].'index.php/';
 $config['url_link'] = $config['base_link'];

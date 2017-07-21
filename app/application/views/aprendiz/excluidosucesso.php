1bbf97a329f887e4a02dfa4b9beb22c4d43186d8
Carlos Eduardo Rosar Kós Lassance <cadurosar@gmail.com>
2017-07-21 10:58:52 -0300

    Revert "Revert "Implementação do CRUD Aprendiz para o site""
    This reverts commit 78f8710393542083f1cd12f04e733cfc43a8c6df.

--- app/application/views/aprendiz/excluidosucesso.php
+++ app/application/views/aprendiz/excluidosucesso.php
@@ -1,5 +1,6 @@
+<script>function goBack() { window.history.back() } </script>
 <?php
-	echo "Aprendiz excluído com sucesso !";
+	echo "Aprendiz excluído com sucesso!";
 	echo "<br>";
-	echo "<a href='{$this->config->item('base_link')}AprendizController/show'>Voltar</a>";
 ?>
+<input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>

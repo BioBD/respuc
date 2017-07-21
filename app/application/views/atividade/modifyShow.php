7a697bd3b538c48b43380f73208932dd76e91cd1
Carlos Eduardo Rosar Kós Lassance <cadurosar@gmail.com>
2017-07-21 10:58:52 -0300

    Revert "Revert "Fix atividade to use only 1 view to show/edit/insert""
    This reverts commit 4a2d4c21d569b2d203fb42b35e26cdb1338509e1.

--- app/application/views/atividade/modifyShow.php
+++ app/application/views/atividade/modifyShow.php
@@ -97,20 +97,88 @@
 
 <div class="container"><br>
   <h4><i class="fa fa-cog" aria-hidden="true"></i>  Inserir Nova Atividade <i class="fa fa-cog" aria-hidden="true"></i></h4><br>
-    <form method="post" id="theForm" action="save" enctype="multipart/form-data">
+    <form method="post" id="theForm" action="<?=$action?>" enctype="multipart/form-data">
       <div class="form-group">
+        <?php if (isset($atividade)) 
+            {
+        ?>
+                <input type="hidden" name="old_nome" id="old_nome" value="<?=$atividade->getNome()?>">
+        <?php    
+            }
+        ?>
         <label for="nome" class="col-lg-1">Nome*: </label><br><br>
-        <input type="text" size="10" class="form-control" placeholder="Digite o nome da atividade"
+        <input type="text" size="10" <?php if (isset($disabled)) echo "disabled";?> class="form-control" placeholder="Digite o nome da atividade"
                   name="nome" onkeypress="return validateLetterInput(event);" required
                   oninvalid="this.setCustomValidity('Este campo não pode ficar vazio.')"
                   oninput="setCustomValidity('')"
                   value="<?php
                           if (!empty($_POST['nome']))
                                 echo $_POST['nome'];
+                          else if(isset($atividade))
+                                echo $atividade->getNome();
+
         ?>"/> </div>
 
     <div class="row">
       <div class="form-group">
         <div class="col-lg-1"></div>
         <input type="button" onclick="goBack()" class='btn btn-warning' value="Voltar"></a>
-        <input class="btn btn-primary" type="submit" value="Salvar"> </div></div></form></div></div>
+        <input class="btn btn-primary" type="submit" <?php if (isset($disabled)) echo "disabled";?> value="Salvar"> 
+        </div>
+    </div>
+    </form>
+
+      <br><br>
+      <br><br>
+        <?php if (isset($pessoas))
+        {
+        ?>
+
+		<table class="table table-bordered table-striped table-min-td-size" style="max-width: 100%;" id="sortable-table">
+							<tr>
+								<th style="    text-align:center;    vertical-align:middle;" colspan="4">Participantes da atividade</th>
+							</tr>
+							<tr>
+                                <th>Nome</th>
+                                <th>Tipo</th>
+                                <th>Remover</th>
+							</tr>
+							<tbody id="tablebody">
+								<?php foreach ($pessoas as $pessoa) { ?> 
+									<tr>
+                                        <?php if($pessoa->tipo === "aluno") 
+                                        {?>
+
+										<td>
+											<a href='<?=$this->config->item('base_link')?>AlunoController/get?cpf=<?=urlencode($pessoa->cpf)?>'>
+												<?=$pessoa->nome?> </a> 
+										</td>
+
+                                        <?php
+                                        }else {?>
+
+										<td>
+											<a href='<?=$this->config->item('base_link')?>AprendizController/get?cpf=<?=urlencode($pessoa->cpf)?>'>
+												<?=$pessoa->nome?> </a> 
+										</td>
+
+
+                                        <?php
+                                        }
+                                        ?>
+										<td><?php echo $pessoa->tipo?></td>
+                                        <td><a href="<?php 
+                                        echo $this->config->item('base_link').'AtividadeController/removePerson?tipo='.$pessoa->tipo.'&cpf='.$pessoa->cpf.'&atividade='.$atividade->getNome(); 
+                                        ?>">
+                                            <input onclick="return confirm('Tem certeza que deseja remover a pessoa de CPF <?= $pessoa->cpf?> dessa atividade ?')" class="btn btn-danger" type="button" value="Deletar"></a></td>								
+									</tr>
+								<?php } ?>
+							</tbody>
+			</table>
+        <?php 
+        }
+        ?>
+
+
+    </div>
+    </div>
